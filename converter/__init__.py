c0aaae2f387cc183a4a496aa7794399e16a8cf7d
User <user@example.com>
2026-02-24 13:07:01 +0300

    feat: add pandoc converter with LaTeX/Mermaid support

--- converter/__init__.py
+++ converter/__init__.py
@@ -1,3 +1,9 @@
 from .secure_cleanup import secure_temp_dir, cleanup_old_temp_dirs
+from .pandoc import convert_md_to_pdf, ConversionError
 
-__all__ = ["secure_temp_dir", "cleanup_old_temp_dirs"]
+__all__ = [
+    "secure_temp_dir",
+    "cleanup_old_temp_dirs",
+    "convert_md_to_pdf",
+    "ConversionError",
+]
